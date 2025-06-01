bca47b14a19ecd03fd6e99dcefd6a61830f52c88
Doriane <dorianemagne20@gmail.com>
2025-06-01 09:27:55 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,2 @@
+from generate_voice import generiere_stimme
+generiere_stimme("Dies ist ein Test.", "testausgabe.wav")
---
+++ test_clone.py
@@ -0,0 +1,3 @@
+from generate_voice_clone import generiere_stimme
+
+generiere_stimme("prof1", "Dies ist eine geklonte Stimme.", "prof1_test.wav")
